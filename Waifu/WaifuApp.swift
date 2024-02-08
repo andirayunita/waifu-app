012b96e0c98b5ba7a50b8d9d99cb4f6252b6c92b
Andira Yunita <andhirayp@gmail.com>
2024-02-08 19:34:56 +0700

    Initial Commit

---
+++ Waifu/WaifuApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaifuApp.swift
+//  Waifu
+//
+//  Created by Andira Yunita on 08/02/24.
+//
+
+import SwiftUI
+
+@main
+struct WaifuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
